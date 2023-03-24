9054568064f7c9f65301e2cd5a824ae5df0d1bf5
ANDREWekpenyong <128842193+ANDREWekpenyong@users.noreply.github.com>
2023-03-25 00:02:01 +0400

    Add files via upload

---
+++ school.js
@@ -0,0 +1,93 @@
+let schools =[
+
+    {
+        "id": 1,
+        "title": "Artificial intelligence ",
+        "location":"United States",
+        "price": 100,
+        "image": "image/arti.jpg",
+        "availableInventory": 5
+       
+      }, 
+      {
+        "id": 2,
+        "title": "Neuroscience",
+        "location":"United Kingdom",
+        "price": 150,
+        "image": "image/neuro.jpg",
+        "availableInventory": 5
+       
+      }, 
+      {
+        "id": 3,
+        "title": "Cybersecurity",
+        "location":"Canada",
+        "price": 80,
+        "image": "image/cyber.jpg",
+        "availableInventory": 5
+       
+      }, 
+      {
+        "id": 4,
+        "title": "Environmental science",
+        "location":"Australia",
+        "price": 100,
+        "image": "image/envi.jpg",
+        "availableInventory": 5
+       
+      }, 
+      {
+        "id": 5,
+        "title": "Linguistics",
+        "location":"Germany",
+        "price": 150,
+        "image": "image/ling.jpg",
+        "availableInventory": 5
+       
+      }, 
+      {
+        "id": 5,
+        "title": "Anthropology ",
+        "location":"France",
+        "price": 90,
+        "image": "image/anth.jpg",
+        "availableInventory": 5
+       
+      }, 
+      {
+        "id": 7,
+        "title": "Quantum mechanics",
+        "location":" Switzerland",
+        "price": 90,
+        "image": "image/quan.jpg",
+        "availableInventory": 5
+       
+      }, 
+      {
+        "id": 8,
+        "title": "Epidemiology",
+        "location":" Japan",
+        "price": 85,
+        "image": "image/epid.jpg",
+        "availableInventory": 5
+       
+      }, 
+      {
+        "id": 9,
+        "title": "Astrophysics ",
+        "location":"Singapore",
+        "price": 100,
+        "image": "image/astro.jpg",
+        "availableInventory": 5
+       
+      }, 
+      {
+        "id":10,
+        "title": "Behavioral economics",
+        "location":"Netherlands",
+        "price": 100,
+        "image": "image/behavi.jpg",
+        "availableInventory": 5
+       
+      }, 
+]
